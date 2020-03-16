226cf086ec9afad9cdb421d5c6f4dbe547b1f168
GerritHoskins <39583066+GerritHoskins@users.noreply.github.com>
2020-03-16 21:33:13 +0000

    nb5

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ import createStore from "./createStore";
 import Todos from "./Todos";
 
 const fbConfig = {
-  apiKey: "AIzaSyCTUERDM-Pchn_UDTsfhVPiwM4TtNIxots",
-  authDomain: "redux-firebasev3.firebaseapp.com",
-  databaseURL: "https://redux-firebasev3.firebaseio.com",
-  storageBucket: "redux-firebasev3.appspot.com",
-  messagingSenderId: "823357791673"
+  apiKey: "AIzaSyBJj_sA3OqXQGrm2UmSUvbTKKp34b42u6Y",
+  authDomain: "dbtronics-79c66.firebaseapp.com",
+  databaseURL: "https://dbtronics-79c66.firebaseio.com",
+  projectId: "dbtronics-79c66",
+  storageBucket: "dbtronics-79c66.appspot.com",
+  messagingSenderId: "299370403853",
+  appId: "1:299370403853:web:2f2a63607f773821259d44"
 };
 
 try {
