b379d21b5df701403aa1f8e0ae177e5087398943
GerritHoskins <gerrit.hoskins@gmail.com>
2020-03-17 21:16:55 +0100

    n8

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {React} from 'react';
+import React, { Component } from 'react';
 import { render } from "react-dom";
 import createStore from "./stores/createStore";
 import { Provider } from "react-redux";
@@ -7,13 +7,9 @@ import "firebase/database";
 import Todos from "./reducers/todos";
 import Footer from "./components/Footer";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
-
 import * as serviceWorker from './createServiceWorker';
 
-
-
-
- const fbConfig = {
+const fbConfig = {
   apiKey: "AIzaSyBJj_sA3OqXQGrm2UmSUvbTKKp34b42u6Y",
   authDomain: "dbtronics-79c66.firebaseapp.com",
   databaseURL: "https://dbtronics-79c66.firebaseio.com",
@@ -23,39 +19,48 @@ import * as serviceWorker from './createServiceWorker';
   appId: "1:299370403853:web:2f2a63607f773821259d44"
 };
 
-try {
-  firebase.initializeApp(fbConfig);
-} catch (err) {
-  console.warn("no fb init.");
+class App extends Component {
+
+  state = {
+    isInit: false
+  }
+
+  onComponentDidMount() {
+    try {
+      firebase.initializeApp(fbConfig);
+      this.setState({ isInit : true });
+    } catch (err) {
+      console.warn("no fb init.");
+    }
+  }
+
+  render(){
+
+    const store = null;
+    const rrfProps = null;
+
+    if(this.state.isInit) {
+      store = createStore();
+      rfProps = {
+          firebase,
+          config: { userProfile: "users" },
+          dispatch: store.dispatch
+      }; 
+    } 
+
+    return (
+      store && rrfProps &&
+        <Provider store={store}>
+                <ReactReduxFirebaseProvider {...rrfProps}>
+                  <div style={styles}>
+                    <Todos />
+                    <Footer />
+                  </div>npm asudit fix 
+                </ReactReduxFirebaseProvider>
+              </Provider>,
+            document.getElementById('root')              
+    )
+      };
 }
 
-const store = createStore();
-
-const rrfProps = {
-  firebase,
-  config: { userProfile: "users" },
-  dispatch: store.dispatch
-};
-
-const styles = {
-  fontFamily: "sans-serif",
-  textAlign: "center"
-};
-
-render(
-    <Provider store={store}>
-        <ReactReduxFirebaseProvider {...rrfProps}>
-          <div style={styles}>
-          <Todos />
-            <Footer />
-          </div>
-        </ReactReduxFirebaseProvider>
-      </Provider>,
-    document.getElementById('root'),
-  );
-  
-  // If you want your app to work offline and load faster, you can change
-  // unregister() to register() below. Note this comes with some pitfalls.
-  // Learn more about service workers: http://bit.ly/CRA-PWA
- serviceWorker.unregister();
-
+export default App;
